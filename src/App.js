babc7c81f744b63e9752a6b00aeb9329a3e08a61
MeqdamAlqudah <memuq56@gmail.com>
2022-01-05 21:17:46 +0200

    Change the App component to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { React, Component } from 'react';
 import Calculator from './components/Calculator';
-
-function App() {
-  return (
-    <div className="container">
-      <Calculator />
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
 export default App;
