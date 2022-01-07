4e59a149504c35bb43e3c9887613dfc11d301e73
MeqdamAlqudah <memuq56@gmail.com>
2022-01-07 15:32:31 +0200

    Change the class component to function component

--- src/components/Buttons/Input.js
+++ src/components/Buttons/Input.js
@@ -1,12 +1,9 @@
 import PropTypes from 'prop-types';
-import { React, Component } from 'react';
 import classes from './Input.module.css';
-// eslint-disable-next-line react/prefer-stateless-function
-class Input extends Component {
-  render() {
-    const { value } = this.props;
-    return (<p className={classes.input}>{value}</p>);
-  }
+
+function Input(props) {
+  const { value } = props;
+  return (<p className={classes.input}>{value}</p>);
 }
 
 Input.propTypes = {
