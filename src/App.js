43763a1b053fdebf8b75a56ceacdbfbb7ad15c86
MeqdamAlqudah <memuq56@gmail.com>
2022-01-14 00:31:43 +0200

    Change the links to NavLink

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Pages/Home';
+import Quote from './components/Pages/Quote';
 
 function App() {
   return (
-    <div className="container">
-      <Calculator />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <Calculator />
+        )}
+        exact
+      />
+      <Route
+        path="/Home"
+        element={
+          <Home />
+        }
+        exact
+      />
+      <Route
+        path="/Quote"
+        element={
+          <Quote />
+        }
+        exact
+      />
+
+    </Routes>
   );
 }
 
